ecaaa7d1bd4f0844072d6281ac433915fcc02ccb
Wayfinder-Ops <ops@rewayfinder.com>
2026-07-31 16:57:48 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -5,15 +5,18 @@ import { getAnalytics } from "firebase/analytics";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optionalconst firebaseConfig = {
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
   apiKey: "AIzaSyDRWQYxeV2Hkize1Py7O4D-h2Av14sqMj4",
   authDomain: "wayfinder-onboarding.firebaseapp.com",
+  databaseURL: "https://wayfinder-onboarding-default-rtdb.firebaseio.com",
   projectId: "wayfinder-onboarding",
   storageBucket: "wayfinder-onboarding.firebasestorage.app",
   messagingSenderId: "105454511155",
   appId: "1:105454511155:web:1cdc4b1294c7557fb46f46",
+  measurementId: "G-F2NWFG505B"
 };
 
-firebase.initializeApp(firebaseConfig);
-const auth = firebase.auth();
-const db = firebase.firestore();
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
